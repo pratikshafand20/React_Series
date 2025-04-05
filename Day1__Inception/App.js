f7e2411db25f8ab79fe545a3930cd9de9a14c2a9
pratikshafand20 <fandpratiksha20@gmail.com>
2025-04-05 09:22:51 +0530

    Day_3_Laying_Then_foundation

--- Day1__Inception/App.js
+++ Day1__Inception/App.js
@@ -20,6 +20,8 @@ root.render(heading);
       </div>
     </div>
     */
+
+/*
 //Nested react element
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
@@ -30,3 +32,42 @@ const heading = React.createElement("div", { id: "parent" }, [
 console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+*/
+
+/*
+//React element
+const heading1 = (
+  <h1 className="heading">
+    <h1>Namaste React using JSX</h1>;
+  </h1>
+);
+//JSX=>Babel converts the JSX to React.createElement=>ReactElement=>JS Object=>HTML Element(render)
+const heading = <h1>Namaste React using JSX</h1>;
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+*/
+
+//React Component
+//Functional component
+
+//Component composition: Nesting one component to another
+const Title = () => (
+  <h1 className="title">
+    <h1>Namaste React using JSX</h1>
+  </h1>
+);
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    {Title()}
+    {/* //We can render any javascript inside the component using the curly
+    braces{}
+    //Title()- we are able to render component like function beacause the title
+    is js function at the end 
+    we can render any react element into component or any component into react element*/}
+    <h1 className="heading">Namaste React functional component🚀❤️</h1>
+  </div>
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
