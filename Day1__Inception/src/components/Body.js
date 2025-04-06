b03ce2a54025ccfb8377b8d0b13d94c469579855
pratikshafand20 <fandpratiksha20@gmail.com>
2025-04-06 23:38:10 +0530

    Episode_5_Filter_RestaurantCards_using_usestate_Hooks

---
+++ Day1__Inception/src/components/Body.js
@@ -0,0 +1,51 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData.js";
+import { useState } from "react";
+
+const Body = () => {
+  //local state variable--superpowerful variable given by react-useState
+  const [listOfRestaurant, setlistOfRestaurant] = useState(resList);
+
+  //above useState hook is destructing on the fly the reslist array of object: like below Array Destructuring
+  // const [arr]=useState([resList]);
+  // const [listOfRestaurant, setlistOfRestaurant]=arr;
+  // const listOfRestaurant=arr[0];
+  // const setlistOfRestaurant=arr[1];
+  console.log(listOfRestaurant);
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurant.filter(
+              (res) => res.card.card.info.avgRatingString > 4.5
+            );
+
+            setlistOfRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurant
+        </button>
+      </div>
+      <div className="res-container">
+        {/* //Rendering Restaurant Cards dynamically from swiggy API */}
+        {listOfRestaurant.map((restaurant) => (
+          <RestaurantCard
+            key={restaurant.card.card.info.id}
+            resData={restaurant}
+          />
+        ))}
+        {/* Below passing props to component but manually */}
+        {/* <RestaurantCard
+          resName="Dominozz "
+          cusinses="Pizza"
+          rating="4.5"
+          address="Akurdi Railway Station"
+        /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
